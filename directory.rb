05487e3dc497bf118f28f0d36b4cde085d79336f
seanbanford <seanbanford@gmail.com>
2019-03-18 09:34:55 +0000

    Approve Code - Array

--- directory.rb
+++ directory.rb
@@ -1,16 +1,29 @@
-student_count = 11
+
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched"
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+]
 
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-puts "Overall, we have #{student_count} great students"
+puts student[0]
+puts student[1]
+puts student[2]
+puts student[3]
+puts student[4]
+puts student[5]
+puts student[6]
+puts student[7]
+puts student[8]
+puts student[9]
+puts student[10]
+puts "Overall, we have #{students.count} great students"
