04d7dd272e6200535096ca2d0f286bae737da250
seanbanford <seanbanford@gmail.com>
2019-03-30 10:30:20 +0000

    Saving data to file

--- directory.rb
+++ directory.rb
@@ -13,21 +13,58 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 =end
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I son't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list of students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list(@students)
+  print_footer(@students)
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
+  @students = []
 
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
 
-  students
+  @students
 
 end
 
@@ -36,9 +73,9 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list(students)
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -46,7 +83,24 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+interactive_menu
+
+
+
+
+
+
+
+
+
+#
